e5357f83f689e7efc362371327b34db2af078f07
entl <maxvorobyov01@gmail.com>
2023-02-25 14:32:22 +0000

    Enum for userQuest table

---
+++ src/Database/UserQuestsColumns.java
@@ -0,0 +1,11 @@
+package Database;
+
+public enum UserQuestsColumns
+{
+    USER_ID(0),
+    QUEST_ID(1);
+
+    public final int value;
+
+    private UserQuestsColumns(int value){this.value = value;}
+}
