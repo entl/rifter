c05169a9597178d98bcc1b47f7a9f3c3e83c6440
entl <maxvorobyov01@gmail.com>
2023-03-07 13:47:03 +0000

    Constant class added

---
+++ src/Constants.java
@@ -0,0 +1,10 @@
+//separate class for constants to access them easily and store in on the one place
+public class Constants
+{
+    protected static final String ACCOUNTS_CSV = "accounts.csv";
+    protected static final String FRIENDS_CSV = "friends.csv";
+    protected static final String TRANSACTIONS_CSV = "transactions.csv";
+    protected static final String ITEMS_FILE = "items.csv";
+    protected static final String QUESTS_CSV = "quests.csv";
+    protected static final String USES_QUESTS_CSV = "userQuests.csv";
+}
