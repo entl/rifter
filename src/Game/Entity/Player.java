060292b6f1c064f86e5fe018e68e6ca402923d55
entl <maxvorobyov01@gmail.com>
2023-03-22 09:26:03 +0000

    Quest interaction added

--- src/Game/Entity/Player.java
+++ src/Game/Entity/Player.java
@@ -1,28 +1,43 @@
 package Game.Entity;
 
+import Database.Account;
+import Database.QuestColumns;
+import Database.Quests;
 import Game.GamePanel;
 import Game.KeyHandler;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Player extends Entity
 {
     GamePanel gamePanel;
     KeyHandler keyHandler;
+    Quests quests;
+    Account user;
     public final int screenX;
     public final int screenY;
 
 
-    public Player(GamePanel gamePanel, KeyHandler keyHandler)
+    public Player(GamePanel gamePanel, KeyHandler keyHandler, Quests quests, Account user)
     {
         this.gamePanel = gamePanel;
         this.keyHandler = keyHandler;
+        this.quests = quests;
+        this.user = user;
 
         screenX = gamePanel.screenWidth/2;
         screenY = gamePanel.screenHeight/2;
 
-        //set base value otherwise null exception
+        //create rectangle smaller than player tile
+        collisionArea = new Rectangle(8, 16, 32, 32);
+        collisionAreaDefaultX = 8;
+        collisionAreaDefaultY = 16;
+
+        //set base direction otherwise null exception
         this.direction = "down";
         setDefaultValues();
         getPlayerImage();
@@ -30,25 +45,43 @@ public class Player extends Entity
 
     public void setDefaultValues()
     {
-        this.worldX = 100;
-        this.worldY = 100;
-        this.speed = 4;
+        this.worldX = 3000;
+        this.worldY = 800;
+        this.speed = 1;
     }
 
     public void getPlayerImage()
     {
-        this.up1 = new ImageIcon("res\\player\\up1.png").getImage();
-        this.up2 = new ImageIcon("res\\player\\up2.png").getImage();
-        this.down1 = new ImageIcon("res\\player\\down1.png").getImage();
-        this.down2 = new ImageIcon("res\\player\\down2.png").getImage();
-        this.left1 = new ImageIcon("res\\player\\left1.png").getImage();
-        this.left2 = new ImageIcon("res\\player\\left2.png").getImage();
-        this.right1 = new ImageIcon("res\\player\\right1.png").getImage();
-        this.right2 = new ImageIcon("res\\player\\right2.png").getImage();
-        this.standdown = new ImageIcon("res\\player\\standdown.png").getImage();
-        this.standup = new ImageIcon("res\\player\\standup.png").getImage();
-        this.standleft = new ImageIcon("res\\player\\standleft.png").getImage();
-        this.standright = new ImageIcon("res\\player\\standright.png").getImage();
+        if (user.getSex().equals("m"))
+        {
+            this.up1 = new ImageIcon("res\\player\\up1.png").getImage();
+            this.up2 = new ImageIcon("res\\player\\up2.png").getImage();
+            this.down1 = new ImageIcon("res\\player\\down1.png").getImage();
+            this.down2 = new ImageIcon("res\\player\\down2.png").getImage();
+            this.left1 = new ImageIcon("res\\player\\left1.png").getImage();
+            this.left2 = new ImageIcon("res\\player\\left2.png").getImage();
+            this.right1 = new ImageIcon("res\\player\\right1.png").getImage();
+            this.right2 = new ImageIcon("res\\player\\right2.png").getImage();
+            this.standdown = new ImageIcon("res\\player\\standdown.png").getImage();
+            this.standup = new ImageIcon("res\\player\\standup.png").getImage();
+            this.standleft = new ImageIcon("res\\player\\standleft.png").getImage();
+            this.standright = new ImageIcon("res\\player\\standright.png").getImage();
+        }
+        else
+        {
+            this.up1 = new ImageIcon("res\\player\\up1_girl.png").getImage();
+            this.up2 = new ImageIcon("res\\player\\up2_girl.png").getImage();
+            this.down1 = new ImageIcon("res\\player\\down1_girl.png").getImage();
+            this.down2 = new ImageIcon("res\\player\\down2_girl.png").getImage();
+            this.left1 = new ImageIcon("res\\player\\left1_girl.png").getImage();
+            this.left2 = new ImageIcon("res\\player\\left2_girl.png").getImage();
+            this.right1 = new ImageIcon("res\\player\\right1_girl.png").getImage();
+            this.right2 = new ImageIcon("res\\player\\right2_girl.png").getImage();
+            this.standdown = new ImageIcon("res\\player\\standdown_girl.png").getImage();
+            this.standup = new ImageIcon("res\\player\\standup_girl.png").getImage();
+            this.standleft = new ImageIcon("res\\player\\standleft_girl.png").getImage();
+            this.standright = new ImageIcon("res\\player\\standright_girl.png").getImage();
+        }
     }
 
     public void update()
@@ -61,27 +94,64 @@ public class Player extends Entity
             if (keyHandler.wPressed)
             {
                 this.direction = "up";
-                this.worldY -= this.speed;
             }
-            else if (keyHandler.sPressed)
+            if (keyHandler.sPressed)
             {
                 this.direction = "down";
-                this.worldY += this.speed;
             }
-            else if (keyHandler.aPressed)
+            if (keyHandler.aPressed)
             {
                 this.direction = "left";
-                this.worldX -= this.speed;
             }
-            else if (keyHandler.dPressed)
+            if (keyHandler.dPressed)
             {
                 this.direction = "right";
-                this.worldX += this.speed;
             }
+
+            //check collision with tiles
+            this.collisionOn = false;
+            this.gamePanel.collisionChecker.checkTile(this);
+
+            //check object collision
+            String objectIndex = gamePanel.collisionChecker.checkObject(this, true);
+            try
+            {
+                if (!objectIndex.equals(""))
+                {
+                    ArrayList<HashMap<String, Object>> userQuests = this.quests.getQuests();
+                    for (HashMap<String, Object> quest:userQuests)
+                    {
+                        if (quest.get(QuestColumns.QUEST_ID.name()).equals(objectIndex))
+                        {
+                            System.out.printf("[+] %s\n",quest.get(QuestColumns.DESCRIPTION.name()));
+                            System.out.printf("[+] You have earned %s and now your balance is %s\n", quest.get(QuestColumns.PRIZE.name()), this.user.getBalance()+ (Float) quest.get(QuestColumns.PRIZE.name()));
+                        }
+                    }
+                    this.quests.addQuestCompleted(objectIndex, this.user);
+                    gamePanel.objects[Integer.parseInt(objectIndex)-1].worldY = -500;
+                }
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+
+            //if no collision let player move
+            if(!this.collisionOn)
+            {
+                switch (direction)
+                {
+                    case "up" -> this.worldY -= this.speed;
+                    case "down" -> this.worldY += this.speed;
+                    case "left" -> this.worldX -= this.speed;
+                    case "right" -> this.worldX += this.speed;
+                }
+            }
+
             this.spriteCounter++;
-            //every 15 fps change sprite num.
-            //in order look like our character walks
-            if (this.spriteCounter > 15)
+            //every 30 fps change sprite num.
+            //in order to look like our character walks
+            if (this.spriteCounter > 30)
             {
                 if (this.spriteNum == 1)
                 {
@@ -94,7 +164,7 @@ public class Player extends Entity
                 this.spriteCounter = 0;
             }
         }
-        //if no key pressed set direction to stand + side to make possible to render image
+        //if no key pressed set standing image based on last direction
         else
         {
             switch (this.direction)
@@ -109,9 +179,8 @@ public class Player extends Entity
     }
     public void draw(Graphics2D graphics2D)
     {
-
+        //draw walking animation
         Image image = null;
-
         switch (this.direction)
         {
             case "up" ->
@@ -163,6 +232,7 @@ public class Player extends Entity
             case "standleft" -> image = this.standleft;
             case "standright" -> image = this.standright;
         }
+        //draw image of player that was chosen in switch case block
         graphics2D.drawImage(image, this.screenX, this.screenY, gamePanel.tileSize, gamePanel.tileSize, null);
     }
 }
